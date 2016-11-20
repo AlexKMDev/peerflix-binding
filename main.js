63a8b276c7a83be071123570ed3282c44a3b9b03
Alexey Metz <root@anakros.me>
2016-11-21 02:04:28 +0700

    add addon files

---
+++ main.js
@@ -0,0 +1,30 @@
+var simple_prefs = require("sdk/simple-prefs");
+const {Cc, Ci} = require("chrome");
+var contextMenu = require("sdk/context-menu");
+
+function play(url) {
+  var file = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsIFile);
+  file.initWithPath(simple_prefs.prefs.player);
+
+  var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
+  process.init(file);
+
+  var params = simple_prefs.prefs.params;
+  var args = [url]
+
+  if (params) {
+    args.push(params.split(" "))
+  }
+
+  process.runAsync(args, args.length);
+}
+
+var menuItem = contextMenu.Item({
+  label: "Watch with Peerflix",
+  context: contextMenu.SelectorContext("[href]"),
+  contentScript: 'self.on("click", function(node, data) {' + 
+                 '  self.postMessage(node.href);' +
+                 '})',
+  accessKey: "e",
+  onMessage: play
+});
